4e83fe46f0d3711d23d2b3bdef7017128363fd74
ashishvartak <vartak.a@gmail.com>
2022-04-19 20:37:53 +0530

    commit

--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
+const dynamicCacheName = 'site-dynamic-v1';
 // install event
 self.addEventListener('install', evt => {
   console.log('service worker installed');
@@ -10,5 +11,15 @@ self.addEventListener('activate', evt => {
 
 // fetch event
 self.addEventListener('fetch', evt => {
-  console.log('fetch event', evt);
+  //console.log('fetch event', evt);
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request).then(fetchRes => {
+        return caches.open(dynamicCacheName).then(cache => {
+          cache.put(evt.request.url, fetchRes.clone());
+          return fetchRes;
+        })
+      });
+    })
+  );
 });
